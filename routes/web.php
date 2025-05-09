cc41950b7803f6ac6db6c98359b39aa40a247d57
s11o-4 <rbnsilo@gmail.com>
2025-05-08 21:26:48 -0600

    basic auth with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,42 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', \App\Http\Controllers\ProductsController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('orders', \App\Http\Controllers\OrdersController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/orders', [OrdersController::class, 'index'])->name('orders.index');
+    Route::get('/orders/create', [OrdersController::class, 'create'])->name('orders.create');
+    Route::post('/orders', [OrdersController::class, 'store'])->name('orders.store');
+    Route::get('/orders/{order}', [OrdersController::class, 'show'])->name('orders.show');
+    Route::get('/orders/{order}/edit', [OrdersController::class, 'edit'])->name('orders.edit');
+    Route::put('/orders/{order}', [OrdersController::class, 'update'])->name('orders.update');
+    Route::delete('/orders/{order}', [OrdersController::class, 'destroy'])->name('orders.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductsController::class, 'create'])->name('products.create');
+    Route::post('/products', [ProductsController::class, 'store'])->name('products.store');
+    Route::get('/products/{order}', [ProductsController::class, 'show'])->name('products.show');
+    Route::get('/products/{order}/edit', [ProductsController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{order}', [ProductsController::class, 'update'])->name('products.update');
+    Route::delete('/products/{order}', [ProductsController::class, 'destroy'])->name('products.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
